faa76736c492524d14526b543b7d6dc89a9baeb8
dhirac <pjdhirajshrestha@gmail.com>
2021-07-21 13:47:50 +0100

    Update application.py

--- application.py
+++ application.py
@@ -276,7 +276,7 @@ api.add_resource(sentiment,'/sentiment', methods = ['POST'])
 
 
 if __name__ == '__main__':
-    application.run(debug=True)
+    application.run(host="0.0.0.0",port=80,debug=True)
 
 
 ##########################################################################
